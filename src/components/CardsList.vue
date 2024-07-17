b0a0e74b7f4a170d6db63f8c4a15a53886286f4a
zhuhao <zhuhaodsr@outlook.com>
2024-07-17 16:25:18 +0800

    initiation

---
+++ src/components/CardsList.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+interface Site {
+  name: string
+  titleEN: string
+  titleCN: string
+  description: string
+  labels: Array<string>
+  site: string
+  image: string
+}
+// 定义 props 类型
+interface Props {
+  sites: Array<Site>
+}
+const props = defineProps<Props>()
+// 跳转目标网站
+function handleJump(site) {
+  window.open(site.site, "blank")
+}
+</script>
+<template>
+  <div class="row">
+    <div class="col" v-for="(site, index) in props.sites" :key="index">
+      <div class="card" @click="handleJump(site)" :title="site.description">
+        <div class="title">
+          <p class="ellipsis1">{{ site.titleEN }}</p>
+          <p class="ellipsis1">{{ site.titleCN }}</p>
+        </div>
+        <div class="content">
+          <!-- <span>{{ site.description }}</span> -->
+          <ul class="labels">
+            <li v-for="(label, index) in site.labels" :key="index">
+              <span class="label">
+                <span class="label-text">{{ label }}</span>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <!-- <img :src="site.image" alt="Site Image" /> -->
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.row {
+  display: flex;
+  // justify-content: center;
+  flex-wrap: wrap;
+  width: 100vw;
+  padding: 0 4vmin;
+  .col {
+    flex: 0 0 25%;
+    width: 25%;
+    padding: 4vmin;
+    .card {
+      display: flex;
+      flex-direction: column;
+      // height: 40vmin;
+      padding: 3vmin;
+      border-radius: 2vmin;
+      color: $--color-nature;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      background-color: rgba($--color-nature-light, 0.6);
+      transition: transform 0.3s;
+      cursor: pointer;
+      &:hover {
+        transform: scale(1.1);
+      }
+      .title {
+        font-weight: bold;
+        font-size: $--size-small;
+      }
+      .content {
+        // flex: 1;
+        margin-top: 2vmin;
+      }
+    }
+  }
+}
+.labels {
+  display: flex;
+  flex-direction: row-reverse;
+  li {
+    padding: 0 0.8vmin;
+  }
+  .label {
+    border-radius: 1vmin;
+    padding: 0.2vmin 1vmin;
+    background-color: $--color-primary-light;
+    .label-text {
+      font-size: $--size-mini;
+    }
+  }
+}
+@media (max-width: 1024px) {
+  .row .col {
+    flex: 0 0 50%;
+    width: 50%;
+    .card {
+      // padding: 2vmin;
+    }
+  }
+}
+</style>
---
+++ src/views/tool/Tool.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import sites from "@/assets/json/sites.json"
+import CardsList from "@/components/CardsList.vue"
+
+const sitesList = ref(sites.tool)
+</script>
+<template>
+  <div class="ai">
+    <div class="header">
+      <div class="title">工 具</div>
+    </div>
+    <CardsList :sites="sitesList"></CardsList>
+  </div>
+</template>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 16vmin;
+  .title {
+    font-size: $--size-normal;
+    color: $--color-nature;
+    font-family: $--font-family-title;
+  }
+}
+</style>
